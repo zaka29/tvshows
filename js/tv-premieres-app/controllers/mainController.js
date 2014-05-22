4f164c32ce459d87faf9c5073493a31d4e9e9661
zaka29 <zaka29@gmail.com>
2014-05-22 15:47:01 +1000

    Filter functionality added

--- js/tv-premieres-app/controllers/mainController.js
+++ js/tv-premieres-app/controllers/mainController.js
@@ -1,6 +1,9 @@
 app.controller("mainController", function($scope, $http){
     $scope.apiKey = "124883f8bc587cb74af1ffe1f9d5cba3";
     $scope.results = [];
+    $scope.filterText = null;
+    $scope.availableGenres = [];
+    $scope.genreFilter = null;
     $scope.init = function() {
         //API requires a start date
         var today = new Date();
@@ -17,11 +20,42 @@ app.controller("mainController", function($scope, $http){
                     //Create a date string from the timestamp so we can filter on it based on user text input
                     tvshow.date = date; //Attach the full date to each episode
                     $scope.results.push(tvshow);
+                    //Loop through each genre for this episode
+                    angular.forEach(tvshow.show.genres, function(genre, index){
+                        //Only add to the availableGenres array if it doesn't already exist
+                        var exists = false;
+                        angular.forEach($scope.availableGenres, function(avGenre, index){
+                            if (avGenre == genre) {
+                                exists = true;
+                            }
+                        });
+                        if (exists === false) {
+                            $scope.availableGenres.push(genre);
+                        }
+                    });
                 });
             });
         }).error(function(error) {
  
         });
+    };
+});
+
+app.filter('isGenre', function() {
+    return function(input, genre) {
+        if (typeof genre == 'undefined' || genre == null) {
+            return input;
+        } else {
+            var out = [];
+            for (var a = 0; a < input.length; a++){
+                for (var b = 0; b < input[a].show.genres.length; b++){
+                    if(input[a].show.genres[b] == genre) {
+                        out.push(input[a]);
+                    }
+                }
+            }
+            return out;
+        }
     };
 });
 
